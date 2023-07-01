23a483060d98028d037a53b6bcf024e51d8267c9
Cristian Franco <cfrancobedoya@gmail.com>
2023-07-01 18:07:39 -0500

    [CI-CD] Add interaction between two modules

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,14 +1,14 @@
-import { Module } from '@nestjs/common';
-
-import { ProductsController } from './controllers/products.controller';
 import { BrandsController } from './controllers/brands.controller';
-import { CategoriesController } from './controllers/categories.controller';
-import { ProductsService } from './services/products.service';
 import { BrandsService } from './services/brands.service';
+import { CategoriesController } from './controllers/categories.controller';
 import { CategoriesService } from './services/categories.service';
+import { Module } from '@nestjs/common';
+import { ProductsController } from './controllers/products.controller';
+import { ProductsService } from './services/products.service';
 
 @Module({
   controllers: [ProductsController, CategoriesController, BrandsController],
   providers: [ProductsService, BrandsService, CategoriesService],
+  exports: [ProductsService],
 })
 export class ProductsModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,12 @@
-import { Module } from '@nestjs/common';
-
 import { CustomerController } from './controllers/customers.controller';
 import { CustomersService } from './services/customers.service';
+import { Module } from '@nestjs/common';
+import { ProductsModule } from 'src/products/products.module';
 import { UsersController } from './controllers/users.controller';
 import { UsersService } from './services/users.service';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule],
   controllers: [CustomerController, UsersController],
   providers: [CustomersService, UsersService],
 })
